3cba3f5f8f7899835ea4cdd8bfa90693b7fc25ed
Arminas Strolys <arminas.strolys@gmail.com>
2022-11-01 16:29:14 +0200

    Added few temp colors for testing, grid layout, padding value for main page layout. Removed placeholder text, image and some other palceholder stuff from index.js. Someone check if I didn't remove useful code.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import classNames from 'classnames/bind';
-import PropTypes from 'prop-types';
-import styles from './styles.module';
-import Svg from '~/assets/svg/image.svg';
 
-const cn = classNames.bind(styles);
-
-export default function App({ prop }) {
-  const charCount = prop.length;
-
+const Index = () => {
   return (
-    <div className={cn('wrapper')}>
-      <div className={cn('content')}>
-        <Svg className={cn('image')} />
-        <pre className={cn('helloworld')} style={{ '--count': charCount }}>
-          {prop}
-        </pre>
+    <>
+      <div className="main-container">
+        <div className="main-container__content-components">
+          Imported components go here!
+        </div>
       </div>
-    </div>
+    </>
   );
-}
-
-App.propTypes = {
-  prop: PropTypes.string,
 };
+
+export default Index;
