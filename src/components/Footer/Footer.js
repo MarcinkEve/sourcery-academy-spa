330ae4d7cbd12fa8b22a3b2b084a13b37acdca81
MarcinkEve <marcinke@gmail.com>
2022-11-02 20:47:05 +0200

    minor layout updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,55 +1,52 @@
 import React from 'react';
-import './footer-style.scss';
 import { FiFacebook, FiTwitter } from 'react-icons/fi';
 import { BsInstagram } from 'react-icons/bs';
 
-const Footer = () => {
+import './footer-style.scss';
+
+export const Footer = () => {
   return (
-    <>
-      <footer className="footer">
-        <div className="footer-container">
-          <ul className="footer-container-list">
-            <li className="footer-container-list--item">
-              <a
-                href="https://www.facebook.com/devbridge/"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="facebook social link"
-                className="footer-container-list--item-link"
-              >
-                <FiFacebook />
-              </a>
-            </li>
-            <li className="footer-container-list--item">
-              <a
-                href="https://twitter.com/devbridge"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="twitter social link"
-                className="footer-container-list--item-link"
-              >
-                <FiTwitter />
-              </a>
-            </li>
-            <li className="footer-container-list--item">
-              <a
-                href="https://www.instagram.com/devbridge"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="instagram social link"
-                className="footer-container-list--item-link"
-              >
-                <BsInstagram />
-              </a>
-            </li>
-          </ul>
-          <p className="footer-container-text">
-            Copyright &copy; 2019 sourcery academy
-          </p>
-        </div>
-      </footer>
-    </>
+    <footer className="footer">
+      <div className="footer-container">
+        <ul className="footer-container-list">
+          <li className="footer-container-list--item">
+            <a
+              href="https://www.facebook.com/devbridge/"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="facebook social link"
+              className="footer-container-list--item-link"
+            >
+              <FiFacebook />
+            </a>
+          </li>
+          <li className="footer-container-list--item">
+            <a
+              href="https://twitter.com/devbridge"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="twitter social link"
+              className="footer-container-list--item-link"
+            >
+              <FiTwitter />
+            </a>
+          </li>
+          <li className="footer-container-list--item">
+            <a
+              href="https://www.instagram.com/devbridge"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="instagram social link"
+              className="footer-container-list--item-link"
+            >
+              <BsInstagram />
+            </a>
+          </li>
+        </ul>
+        <p className="footer-container-text">
+          Copyright &copy; 2019 sourcery academy
+        </p>
+      </div>
+    </footer>
   );
 };
-
-export default Footer;
