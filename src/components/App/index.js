143902ffbcb08eaa178d64153a6942d155a580f2
Arminas Strolys <arminas.strolys@gmail.com>
2022-11-02 19:56:52 +0200

    fixes: unnecessary code removed, code optimization

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
+
 import PageLayout from '../../layout/page-layout/pageLayout';
 
-const Index = () => {
+export default function index() {
   return (
-    <>
-      <PageLayout>
-        <h1 style={{ textAlign: 'center' }}>
-          {' '}
-          &rarr; Drop components here &larr;{' '}
-        </h1>{' '}
-        {/* Delete this h1 tag after first component added */}
-      </PageLayout>
-    </>
+    <PageLayout>
+      <h1 style={{ textAlign: 'center' }}>
+        {' '}
+        &rarr; Drop components here &larr;{' '}
+      </h1>{' '}
+      {/* Delete this h1 tag after adding first component */}
+    </PageLayout>
   );
-};
-
-export default Index;
+}
--- src/layout/page-layout/pageLayout.js
+++ src/layout/page-layout/pageLayout.js
@@ -1,18 +1,17 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
+
 import './pageLayout.scss';
 
-const PageLayout = (props) => {
+export default function pageLayout(props) {
   return (
-    <>
-      <div className="page-layout">
-        <div className="main-container">{props.children}</div>
-      </div>
-    </>
+    <div className="page-layout">
+      <div className="main-container">{props.children}</div>
+    </div>
   );
-};
-PageLayout.propTypes = {
+}
+
+pageLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
-
-export default PageLayout;
