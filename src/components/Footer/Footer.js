bb911e5c0cd81711914e03650ae880aacfbccd4a
MarcinkEve <marcinke@gmail.com>
2022-11-03 22:51:10 +0200

    removed <a> tags, transferred to icons components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,51 +1,26 @@
 import React from 'react';
-import { FiFacebook, FiTwitter } from 'react-icons/fi';
-import { BsInstagram } from 'react-icons/bs';
+import Facebook from '../icons/Facebook';
+import Instagram from '../icons/Instagram';
+import Twitter from '../icons/Twitter';
 
-import './footer-style.scss';
+import './footer.scss';
 
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-container">
-        <ul className="footer-container-list">
-          <li className="footer-container-list--item">
-            <a
-              href="https://www.facebook.com/devbridge/"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="facebook social link"
-              className="footer-container-list--item-link"
-            >
-              <FiFacebook />
-            </a>
+      <div className="footer__container">
+        <ul className="footer__list">
+          <li className="footer__list-item">
+            <Facebook />
           </li>
-          <li className="footer-container-list--item">
-            <a
-              href="https://twitter.com/devbridge"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="twitter social link"
-              className="footer-container-list--item-link"
-            >
-              <FiTwitter />
-            </a>
+          <li className="footer__list-item">
+            <Twitter />
           </li>
-          <li className="footer-container-list--item">
-            <a
-              href="https://www.instagram.com/devbridge"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="instagram social link"
-              className="footer-container-list--item-link"
-            >
-              <BsInstagram />
-            </a>
+          <li className="footer__list-item">
+            <Instagram />
           </li>
         </ul>
-        <p className="footer-container-text">
-          Copyright &copy; 2019 sourcery academy
-        </p>
+        <p className="footer__text">Copyright &copy; 2019 sourcery academy</p>
       </div>
     </footer>
   );
