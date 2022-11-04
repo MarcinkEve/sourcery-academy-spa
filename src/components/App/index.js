aa137763624c94a8d8a172520589b226f958f36d
emilijaMarija <z.mari.emilija1@gmail.com>
2022-11-04 19:15:05 +0200

    SFE22D-27 - Revert App/index.js changes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,40 +3,19 @@ import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import styles from './styles.module';
 import Svg from '~/assets/svg/image.svg';
-import HeaderDropdown from '~/components/Header/Dropdown/HeaderDropdown';
 
 const cn = classNames.bind(styles);
 
 export default function App({ prop }) {
   const charCount = prop.length;
 
-  const sampleData = [
-    {
-      text: 'Text1',
-      event: () => {},
-    },
-    {
-      text: 'Text2',
-      event: () => {},
-    },
-    {
-      text: 'Text3',
-      event: () => {},
-    },
-    {
-      text: 'Text4',
-      event: () => {},
-    },
-  ];
-
   return (
     <div className={cn('wrapper')}>
       <div className={cn('content')}>
-        {/*<Svg className={cn('image')} />*/}
-        {/*<pre className={cn('helloworld')} style={{ '--count': charCount }}>*/}
-        {/*  {prop}*/}
-        {/*</pre>*/}
-        <HeaderDropdown data={sampleData} />
+        <Svg className={cn('image')} />
+        <pre className={cn('helloworld')} style={{ '--count': charCount }}>
+          {prop}
+        </pre>
       </div>
     </div>
   );
