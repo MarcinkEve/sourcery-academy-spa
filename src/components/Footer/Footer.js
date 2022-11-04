cf19f3e400e02ddf4fc6106b8bf69ad8ffcf4b4f
MarcinkEve <marcinke@gmail.com>
2022-11-04 14:17:31 +0200

    FIX: icons passed dinamically, links created dinamically, one "Icon" component for all icons created

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import Facebook from '../icons/Facebook';
-import Instagram from '../icons/Instagram';
-import Twitter from '../icons/Twitter';
+import { FiTwitter, FiFacebook } from 'react-icons/fi';
+import { BsInstagram } from 'react-icons/bs';
 
+import Icon from '../Icon';
 import './footer.scss';
 
 export const Footer = () => {
+  const icons = [
+    {
+      id: 1,
+      title: 'facebook',
+      link: 'https://www.facebook.com/devbridge/',
+      icon: <FiFacebook />,
+    },
+    {
+      id: 2,
+      title: 'twitter',
+      link: 'https://twitter.com/devbridge',
+      icon: <FiTwitter />,
+    },
+    {
+      id: 3,
+      title: 'instagram',
+      link: 'https://www.instagram.com/devbridge',
+      icon: <BsInstagram />,
+    },
+  ];
   return (
     <footer className="footer">
       <div className="footer__container">
         <ul className="footer__list">
-          <li className="footer__list-item">
-            <Facebook />
-          </li>
-          <li className="footer__list-item">
-            <Twitter />
-          </li>
-          <li className="footer__list-item">
-            <Instagram />
-          </li>
+          {icons.map((element, index) => (
+            <li className="footer__list-item" key={index}>
+              <Icon element={element} />
+            </li>
+          ))}
         </ul>
         <p className="footer__text">Copyright &copy; 2019 sourcery academy</p>
       </div>
