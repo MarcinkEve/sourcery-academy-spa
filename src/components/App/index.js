93fceac4d9a435aaa3d50756fea0a8597a20ac64
MarcinkEve <marcinke@gmail.com>
2022-11-05 20:38:20 +0200

    removed Footer component container, function to-em and its file renamed to-rem, changed root element font size, other minor updates

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,21 +3,24 @@ import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import styles from './styles.module';
 import Svg from '~/assets/svg/image.svg';
-
+import Footer from '../Footer';
 const cn = classNames.bind(styles);
 
 export default function App({ prop }) {
   const charCount = prop.length;
 
   return (
-    <div className={cn('wrapper')}>
-      <div className={cn('content')}>
-        <Svg className={cn('image')} />
-        <pre className={cn('helloworld')} style={{ '--count': charCount }}>
-          {prop}
-        </pre>
+    <>
+      <div className={cn('wrapper')}>
+        <div className={cn('content')}>
+          <Svg className={cn('image')} />
+          <pre className={cn('helloworld')} style={{ '--count': charCount }}>
+            {prop}
+          </pre>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,19 +8,16 @@ import './footer.scss';
 export const Footer = () => {
   const icons = [
     {
-      id: 1,
       title: 'facebook',
       link: 'https://www.facebook.com/devbridge/',
       icon: <FiFacebook className="social-link__icon" />,
     },
     {
-      id: 2,
       title: 'twitter',
       link: 'https://twitter.com/devbridge',
       icon: <FiTwitter className="social-link__icon" />,
     },
     {
-      id: 3,
       title: 'instagram',
       link: 'https://www.instagram.com/devbridge',
       icon: <BsInstagram className="social-link__icon" />,
@@ -28,16 +25,14 @@ export const Footer = () => {
   ];
   return (
     <footer className="footer">
-      <div className="footer__container">
-        <ul className="footer__list">
-          {icons.map((element, index) => (
-            <li className="footer__list-item" key={index}>
-              <SocialLink element={element} />
-            </li>
-          ))}
-        </ul>
-        <p className="footer__text">Copyright &copy; 2022 sourcery academy</p>
-      </div>
+      <ul className="footer__list">
+        {icons.map((element, index) => (
+          <li className="footer__list-item" key={index}>
+            <SocialLink element={element} />
+          </li>
+        ))}
+      </ul>
+      <p className="footer__text">Copyright &copy; 2022 sourcery academy</p>
     </footer>
   );
 };
