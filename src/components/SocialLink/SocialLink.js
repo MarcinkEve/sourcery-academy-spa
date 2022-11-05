ee36ad5535bb4da57d78de428938f2e48f153e95
MarcinkEve <marcinke@gmail.com>
2022-11-05 20:45:06 +0200

    SocialLink component minor update

--- src/components/SocialLink/SocialLink.js
+++ src/components/SocialLink/SocialLink.js
@@ -5,17 +5,15 @@ import './socialLink.scss';
 
 export const SocialLink = ({ element }) => {
   return (
-    <>
-      <a
-        href={element.link}
-        target="_blank"
-        rel="noreferrer"
-        aria-label={element.title + ' social link'}
-        className="social-link"
-      >
-        {element.icon}
-      </a>
-    </>
+    <a
+      href={element.link}
+      target="_blank"
+      rel="noreferrer"
+      aria-label={element.title + ' social link'}
+      className="social-link"
+    >
+      {element.icon}
+    </a>
   );
 };
 
