2669274616b04c993a3b896aec9725b786886f2e
emilijaMarija <z.mari.emilija1@gmail.com>
2022-11-10 14:47:09 +0200

    SFE22D-27 - Add shadow to dropdown pin

--- src/components/Header/Dropdown/HeaderDropdown.js
+++ src/components/Header/Dropdown/HeaderDropdown.js
@@ -8,7 +8,9 @@ const cn = classNames.bind(styles);
 const HeaderDropdown = ({ data }) => {
   return (
     <div className={cn('dropdown')}>
-      <div className={cn('dropdown__triangle')} />
+      <div className={cn('dropdown__container')}>
+        <div className={cn('dropdown__container-triangle')} />
+      </div>
       <ul className={cn('dropdown__content')}>
         {data.map((element, index) => (
           <li key={index.toString()} className={cn('dropdown__content-link')}>
