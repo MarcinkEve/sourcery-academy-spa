53de36f396c48a37cabc80c1aa630b5b0ade4d5f
MarcinkEve <marcinke@gmail.com>
2022-11-10 15:37:08 +0200

    <Footer> component location changed

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,13 +5,15 @@ import { Footer } from '../Footer/Footer';
 
 export default function index() {
   return (
-    <PageLayout>
-      <h1 style={{ textAlign: 'center' }}>
-        {' '}
-        &rarr; Drop components here &larr;{' '}
-      </h1>{' '}
-      {/* Delete this h1 tag after adding first component */}
+    <>
+      <PageLayout>
+        <h1 style={{ textAlign: 'center' }}>
+          {' '}
+          &rarr; Drop components here &larr;{' '}
+        </h1>{' '}
+        {/* Delete this h1 tag after adding first component */}
+      </PageLayout>
       <Footer />
-    </PageLayout>
+    </>
   );
 }
