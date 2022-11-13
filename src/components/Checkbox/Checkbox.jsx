4f6f587e02d54dd7d8221a5ba404aabef3a99f03
Mantvydas Liobė <liobem@gmail.com>
2022-11-13 11:41:12 +0200

    added states indicators, custom checked box

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import './Checkbox.scss';
+import './checkbox.scss';
 
 const Checkbox = ({ checkboxText }) => {
   const [isChecked, setIsChecked] = useState(false);
 
   return (
     <label onChange={() => setIsChecked(!isChecked)}>
-      <div className="checkbox">
-        <div className="checkbox__checkmark">
+      <div className="checkbox" tabIndex="0">
+        <div className="checkbox__checkmark--container">
           <input
+            className="checkmark--default"
             checked={isChecked}
             onChange={() => setIsChecked(!isChecked)}
             type="checkbox"
-            name="checked_privacy"
+            name="check_privacy"
           ></input>
+          <div
+            className={
+              isChecked ? 'checkmark--custom checked' : 'checkmark--custom'
+            }
+          ></div>
         </div>
 
         <p className="checkbox__text">{checkboxText}</p>
