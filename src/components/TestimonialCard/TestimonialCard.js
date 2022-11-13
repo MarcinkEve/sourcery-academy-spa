423bdfd79918da3121cbef2a6acf5039f844ae2d
emilijaMarija <z.mari.emilija1@gmail.com>
2022-11-13 17:44:17 +0200

    SFE22D-17 - Use conditional rendering to improve component re-usability

--- src/components/TestimonialCard/TestimonialCard.js
+++ src/components/TestimonialCard/TestimonialCard.js
@@ -2,19 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './TestimonialCard.scss';
 
-export const TestimonialCard = ({ data, theme }) => {
-  const { photo, message, name, academy } = data;
-
+export const TestimonialCard = ({ data, theme = 'home' }) => {
+  const {
+    photo,
+    message,
+    name,
+    academy,
+    alt = 'Graduate profile photo',
+  } = data;
   return (
     <div className="card" theme={theme}>
-      <img className="card__photo" src={photo} alt="Graduate profile photo" />
+      {photo && <img className="card__photo" src={photo} alt={alt} />}
       <div className="card__quote">
         <p className="card__quote-mark">“</p>
         <p className="card__quote-message">{message}</p>
-        <p className="card__quote-mark card__quote-mark-lower">“</p>
+        <p className="card__quote-mark card__quote-mark--lower">“</p>
       </div>
-      <h3 className="card__name">{name}</h3>
-      <h4 className="card__academy">{academy}</h4>
+      {name && <h3 className="card__name">{name}</h3>}
+      {academy && <h4 className="card__academy">{academy}</h4>}
     </div>
   );
 };
@@ -22,15 +27,10 @@ export const TestimonialCard = ({ data, theme }) => {
 TestimonialCard.propTypes = {
   data: PropTypes.shape({
     photo: PropTypes.string,
-    message: PropTypes.string,
+    message: PropTypes.string.isRequired,
     name: PropTypes.string,
     academy: PropTypes.string,
-  }),
-  theme: PropTypes.oneOf([
-    'home',
-    'developers',
-    'testers',
-    'front-end',
-    'application',
-  ]),
+    alt: PropTypes.string,
+  }).isRequired,
+  theme: PropTypes.string,
 };
