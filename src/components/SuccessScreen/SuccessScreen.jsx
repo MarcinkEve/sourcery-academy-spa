500ee19223d562d0618b9c872b40ef70406c1460
Arminas Strolys <arminas.strolys@gmail.com>
2022-11-15 22:49:04 +0200

    Added particles svg.

--- src/components/SuccessScreen/SuccessScreen.jsx
+++ src/components/SuccessScreen/SuccessScreen.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './successScreen.scss';
 import IconCheckMark from '../../assets/icons/icon-check-mark.svg';
 import IconSuccessImage from '../../assets/icons/icon-success-image.svg';
+import IconParticles from '../../assets/icons/icon-particles.svg';
 
 export const SuccessScreen = () => {
   return (
     <div className="success-container">
+      <IconParticles className="success-container__particles-icon" />
       <h1 className="success-container__header">
         Sourcery Academy Application
       </h1>
