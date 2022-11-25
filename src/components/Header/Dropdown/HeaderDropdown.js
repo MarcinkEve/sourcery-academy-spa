aa6144c4ed20d1722ffba4430e2fdf571e3aa467
emilijaMarija <z.mari.emilija1@gmail.com>
2022-11-25 20:48:30 +0200

    SFE22D-27 - Improve header dropdown
    Changes made:
     - Handle click outside of dropdown event in dropdown component
     - Added transition on list states
     - Added escape key handling to close dropdown
     - Tweaked the pin triangle size

--- src/components/Header/Dropdown/HeaderDropdown.js
+++ src/components/Header/Dropdown/HeaderDropdown.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { arrayOf, shape, string } from 'prop-types';
+import { arrayOf, func, shape, string } from 'prop-types';
 
 import './headerDropdown.scss';
 
-export const HeaderDropdown = ({ data }) => {
+export const HeaderDropdown = ({ data, onClickOutside }) => {
+  const pinRef = useRef(null);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (!pinRef.current || !contentRef.current) {
+      return;
+    }
+    const clickListener = ({ target }) => {
+      if (
+        onClickOutside &&
+        !pinRef.current.contains(target) &&
+        !contentRef.current.contains(target)
+      ) {
+        onClickOutside();
+      }
+    };
+    document.addEventListener('mousedown', clickListener);
+    return () => document.removeEventListener('mousedown', clickListener);
+  }, [pinRef, contentRef]);
   return (
     <div className="dropdown">
-      <div className="dropdown__container">
-        <div className="dropdown__container-triangle" />
+      <div className="dropdown__pin">
+        <div className="dropdown__pin-triangle" ref={pinRef} />
       </div>
-      <ul className="dropdown__content">
+      <ul className="dropdown__content" ref={contentRef}>
         {data.map((element, index) => (
           <li key={index} className="dropdown__content-item">
             <Link className="dropdown__content-link" to={element.route}>
@@ -30,4 +49,5 @@ HeaderDropdown.propTypes = {
       route: string.isRequired,
     })
   ),
+  onClickOutside: func,
 };
