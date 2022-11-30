5846ebce565c6e67b9e99b8be7598deeb937f39a
emilijaMarija <z.mari.emilija1@gmail.com>
2022-11-30 15:17:05 +0200

    SFE22D-39 - Update carousel width when resizing window

--- src/components/TestimonialWrapper/TestimonialWrapper.jsx
+++ src/components/TestimonialWrapper/TestimonialWrapper.jsx
@@ -1,61 +1,88 @@
-import React, { useLayoutEffect, useRef, useState} from 'react';
-import {CarouselProvider, Slider, Slide, ButtonBack, ButtonNext} from 'pure-react-carousel';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from 'pure-react-carousel';
+import { arrayOf, shape, string } from 'prop-types';
 
+import TestimonialCard from './TestimonialCard';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import './TestimonialWrapper.scss';
-import {cardWidth, shadowSize} from './TestimonialCard/TestimonialCard.scss'
-import TestimonialCard from './TestimonialCard';
+import { cardWidth, shadowSize } from './TestimonialCard/TestimonialCard.scss';
 import SvgArrow from '../../assets/icons/icon-feather-arrow-down.svg';
-import {arrayOf, shape, string} from 'prop-types';
 
-export const TestimonialWrapper = ({title, data, alt }) => {
-    const ref = useRef(null);
-    const [containerWidth, setContainerWidth] = useState(0);
-
-    useLayoutEffect(() => setContainerWidth(ref.current.offsetWidth), [ref]);
-    const testimonialCardWidth = parseInt(cardWidth) + parseInt(shadowSize);
-    const gap = (containerWidth - testimonialCardWidth * 3) / 2;
-
-    return (
-        <div className="carousel" ref={ref}>
-            {title && <h2 className="carousel__title">{title}</h2>}
-            <CarouselProvider
-                totalSlides={data.length}
-                visibleSlides={3}
-                isIntrinsicHeight={true}
-            >
-                <div className={'carousel__button-container'}>
-                    <ButtonBack className={'carousel__button'}>
-                        <SvgArrow className={'carousel__arrow'}/>
-                    </ButtonBack>
-                    <ButtonNext className={'carousel__button'}>
-                        <SvgArrow className={'carousel__arrow carousel__arrow--next'}/>
-                    </ButtonNext>
-                </div>
-                <Slider className={'carousel__slider'} slideTraySize={(testimonialCardWidth + gap) * data.length * 100 / containerWidth}>
-                    {data.map((element, index) => (
-                        <Slide key={index} index={index} className="carousel__card">
-                            <div style={{marginRight: gap}}>
-                                <TestimonialCard data={data[index]} alt = {alt && alt[index]}/>
-                            </div>
-
-                        </Slide>
-                    ))}
-                </Slider>
-            </CarouselProvider>
-        </div>
-    )
+function useWindowSizeUpdates(func, deps) {
+  useEffect(() => {
+    window.addEventListener('resize', func);
+    return () => window.removeEventListener('resize', func);
+  }, deps);
 }
+
+export const TestimonialWrapper = ({ title, data, alt }) => {
+  const ref = useRef(null);
+  const [containerWidth, setContainerWidth] = useState(0);
+
+  // Set initial width
+  useEffect(() => ref.current && setContainerWidth(ref.current.offsetWidth), [
+    ref,
+  ]);
+  // Update width state when page size changes
+  useWindowSizeUpdates(() => {
+    if (ref.current) {
+      setContainerWidth(ref.current.offsetWidth);
+    }
+  }, [ref]);
+
+  const testimonialCardWidth = parseInt(cardWidth) + parseInt(shadowSize);
+  const gap = (containerWidth - testimonialCardWidth * 3) / 2;
+
+  return (
+    <div className="carousel" ref={ref}>
+      {title && <h2 className="carousel__title">{title}</h2>}
+      <CarouselProvider
+        totalSlides={data.length}
+        visibleSlides={3}
+        isIntrinsicHeight={true}
+      >
+        <div className={'carousel__button-container'}>
+          <ButtonBack className={'carousel__button'}>
+            <SvgArrow className={'carousel__arrow'} />
+          </ButtonBack>
+          <ButtonNext className={'carousel__button'}>
+            <SvgArrow className={'carousel__arrow carousel__arrow--next'} />
+          </ButtonNext>
+        </div>
+        <Slider
+          className={'carousel__slider'}
+          slideTraySize={
+            ((testimonialCardWidth + gap) * data.length * 100) / containerWidth
+          }
+        >
+          {data.map((element, index) => (
+            <Slide key={index} index={index} className="carousel__card">
+              <div style={{ marginRight: gap }}>
+                <TestimonialCard data={data[index]} alt={alt && alt[index]} />
+              </div>
+            </Slide>
+          ))}
+        </Slider>
+      </CarouselProvider>
+    </div>
+  );
+};
 
 TestimonialWrapper.propTypes = {
-    title: string,
-    data: arrayOf(
-        shape({
-            photo: string,
-            message: string.isRequired,
-            name: string,
-            academy: string,
-        }).isRequired,
-    ),
-    alt: arrayOf(string),
-}
+  title: string,
+  data: arrayOf(
+    shape({
+      photo: string,
+      message: string.isRequired,
+      name: string,
+      academy: string,
+    }).isRequired
+  ),
+  alt: arrayOf(string),
+};
