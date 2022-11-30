90ae6aca3b7b5a4466bb69f515dc349ebf303fcf
emilijaMarija <z.mari.emilija1@gmail.com>
2022-11-30 15:07:34 +0200

    SFE22D-39 - Add storybook theme controls for testimonial wrapper

--- src/components/TestimonialWrapper/TestimonialWrapper.stories.jsx
+++ src/components/TestimonialWrapper/TestimonialWrapper.stories.jsx
@@ -1,37 +1,35 @@
 import React from 'react';
 
 import { TestimonialWrapper } from './TestimonialWrapper';
-import {data} from './TestimonialCard/data';
+import { data } from './TestimonialCard/data';
 
 export default {
-    title: 'TestimonialWrapper',
-    component: TestimonialWrapper,
-    args: {
-        title: 'Testimonials',
-        data: data.slice(0,4),
+  title: 'TestimonialWrapper',
+  component: TestimonialWrapper,
+  args: {
+    theme: 'Violet',
+    title: 'Testimonials',
+    data: data.slice(0, 4),
+  },
+  argTypes: {
+    theme: {
+      options: ['Violet', 'Blue', 'Green', 'Red'],
+      control: 'radio',
+      mapping: {
+        Violet: 'home',
+        Blue: 'developers',
+        Green: 'testers',
+        Red: 'front-end',
+      },
     },
+  },
 };
 
-const provideTheme = (theme) => {
-    return [
-        (Story) => (
-            <div theme={theme}>
-                <Story />
-            </div>
-        ),
-    ];
+export const TestimonialWrapperComponent = (args) => {
+  const { theme, ...rest } = args;
+  return (
+    <div theme={theme}>
+      <TestimonialWrapper {...rest} />
+    </div>
+  );
 };
-
-const Template = (args) => <TestimonialWrapper {...args} />;
-
-export const Home = Template.bind({});
-Home.decorators = provideTheme('home');
-
-export const Developers = Template.bind({});
-Developers.decorators = provideTheme('developers');
-
-export const Testers = Template.bind({});
-Testers.decorators = provideTheme('testers');
-
-export const FrontEnd = Template.bind({});
-FrontEnd.decorators = provideTheme('front-end');
