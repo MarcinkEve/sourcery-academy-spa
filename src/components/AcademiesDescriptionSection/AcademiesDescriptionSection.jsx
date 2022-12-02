fc0e2ffa20cd8fdd40e42c8f67ee3cc743a3b44d
Evelina <marcinke@gmail.com>
2022-12-02 19:45:32 +0200

    branch and main wrapper created

---
+++ src/components/AcademiesDescriptionSection/AcademiesDescriptionSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../academies-description-section.scss';
+
+export const AcademiesDescriptionSection = () => {
+  return (
+    <section
+      className="academies-description-section"
+      id="academies-decription-section"
+    >
+      AcademiesDescriptionSection
+    </section>
+  );
+};
