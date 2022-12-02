4bd646d8ca0a106a7bb6c7e9da06ce04d1f15e9c
Arminas Strolys <arminas.strolys@gmail.com>
2022-12-02 10:38:33 +0200

    Still testing...

--- src/Video/Video.jsx
+++ src/Video/Video.jsx
@@ -4,6 +4,7 @@ import './video.scss';
 import CustomPlayPause from './CustomPlayPause';
 import CustomFullscreen from './CustomFullscreen';
 import CustomVolume from './CustomVolume';
+import Settings from '../assets/icons/icon-settings.svg';
 
 const { CurrentTime, SeekBar, MuteUnmute, Volume } = controls;
 
@@ -14,7 +15,8 @@ export const Video = () => {
         <div className="media-player">
           <Player
             src="https://sfe-2022-data.netlify.app/static/video/testers/7cd88093664cd782e4868a6706f2787da2eb7dc9.mp4"
-            loop={true}
+            // controls="controls"
+            // onclick="play()"
           />
         </div>
         <div className="media-controls">
@@ -24,6 +26,9 @@ export const Video = () => {
           {/* <MuteUnmute /> */}
           <CustomVolume className="vol" />
           {/* <Volume className="volume-btn" /> */}
+          <button className="settings-btn">
+            <Settings className="settings-icon" />
+          </button>
           <CustomFullscreen className="full-screen-btn" />
         </div>
       </div>
