f5bbd18a572cc068d6f2b750acc681f7ffd6d191
KlemBut <klemensasbutkevicius@gmail.com>
2022-12-06 17:50:29 +0200

    updated pageLayout to have min height and not overflow, fixed propTypes warning, aded AcademyWrapper, eddited route imports

---
+++ src/components/Pages/AcademiesPages/AcademyWrapper/AcademyWrapper.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {} from 'prop-types';
+import { useLocation } from 'react-router-dom';
+
+export const AcademyWrapper = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      <h1>This is the {location.pathname} academy </h1>
+    </>
+  );
+};
+
+AcademyWrapper.propTypes = {};
