2ad3369cbfe6c776c06004bf9287494ab02ba2a4
emilijaMarija <z.mari.emilija1@gmail.com>
2022-12-08 12:01:08 +0200

    SFE22D-39 - Add an empty line

--- src/components/TestimonialWrapper/TestimonialCard/TestimonialCard.jsx
+++ src/components/TestimonialWrapper/TestimonialCard/TestimonialCard.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { string, shape } from 'prop-types';
+
 import './TestimonialCard.scss';
 
 export const TestimonialCard = ({ data, alt = 'Graduate profile photo' }) => {
   const { photo, message, name, academy } = data;
   return (
-      <div className="card">
-        {photo && <img className="card__photo" src={photo} alt={alt} />}
-        <div className="card__quote">
-          <p className="card__quote-mark">“</p>
-          <p className="card__quote-message">{message}</p>
-          <p className="card__quote-mark card__quote-mark--lower">„</p>
-        </div>
-        {name && <p className="card__name">{name}</p>}
-        {academy && <p className="card__academy">{academy}</p>}
+    <div className="card">
+      {photo && <img className="card__photo" src={photo} alt={alt} />}
+      <div className="card__quote">
+        <p className="card__quote-mark">“</p>
+        <p className="card__quote-message">{message}</p>
+        <p className="card__quote-mark card__quote-mark--lower">„</p>
       </div>
+      {name && <p className="card__name">{name}</p>}
+      {academy && <p className="card__academy">{academy}</p>}
+    </div>
   );
 };
 
@@ -27,4 +28,3 @@ TestimonialCard.propTypes = {
   }).isRequired,
   alt: string,
 };
-
