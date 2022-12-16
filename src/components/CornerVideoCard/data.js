da2cdf6b383d7bd8e6331cb8e7980fed13c2255b
MarcinkEve <marcinke@gmail.com>
2022-12-16 23:21:48 +0200

    deleted some images, data.js file cleaned up

--- src/components/CornerVideoCard/data.js
+++ src/components/CornerVideoCard/data.js
@@ -2,14 +2,6 @@ import DevelopersCornerImage from '~/assets/images/developers-corner-photo.jpg';
 import TestersCornerImage from '~/assets/images/testers-corner-photo.jpg';
 import FrontEndCornerImage from '~/assets/images/frontend-corner-photo.jpg';
 
-// import DevelopersCornerImage from '~/assets/images/developers-corner-photo.png';
-// import TestersCornerImage from '~/assets/images/testers-corner-photo.png';
-// import FrontEndCornerImage from '~/assets/images/frontend-corner-photo.png';
-
-// import DevelopersCornerImage from '~/assets/images/developers-corner-photo.svg';
-// import TestersCornerImage from '~/assets/images/testers-corner-photo.svg';
-// import FrontEndCornerImage from '~/assets/images/frontend-corner-photo.svg';
-
 export const cornerImages = {
   developers: {
     image: DevelopersCornerImage,
