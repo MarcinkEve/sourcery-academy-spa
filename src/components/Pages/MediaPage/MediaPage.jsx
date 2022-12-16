d98112171c7b8c5a6fbcf7ede43ad231bcfdacae
Elena Darškuvienė <e.darskuviene@gmail.com>
2022-12-16 14:58:20 +0200

    updated MediaPage

--- src/components/Pages/MediaPage/MediaPage.jsx
+++ src/components/Pages/MediaPage/MediaPage.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { string } from 'prop-types';
 
 import PageLayout from '~/layout/pageLayout';
+import MediaSection from '~/components/MediaSection';
+import './media-page.scss';
+import mediaData from './data.json';
+
+const filterMedia = (academy) =>
+  mediaData.filter((item) => item.academy === academy);
+
+const mediaDevelopers = filterMedia('developers');
+const mediaTesters = filterMedia('testers');
+const mediaFrontend = filterMedia('frontend');
 
 export const MediaPage = ({ theme }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <PageLayout theme={theme}>
-      {/* Placeholder heading */}
-      <h1 style={{ textAlign: 'center' }}>This is the media page</h1>
+      <div className="media-page-wrapper">
+        <MediaSection title="Sourcery for Developers" media={mediaDevelopers} />
+        <MediaSection title="Sourcery for Testers" media={mediaTesters} />
+        <MediaSection title="Sourcery for Front-end" media={mediaFrontend} />
+      </div>
     </PageLayout>
   );
 };
