75aa8ec5a8459ad015feefd1d5f6cbd2fc84b141
emilijaMarija <z.mari.emilija1@gmail.com>
2022-12-19 10:51:07 +0200

    SFE22D-60 - Fix import sorting and paths

--- src/components/TestimonialSection/Homepage/TestimonialSection.jsx
+++ src/components/TestimonialSection/Homepage/TestimonialSection.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import BottomParticles from '~/assets/particles/particles-testimonial-homepage-bottom.svg';
-import './testimonial-section.scss';
 import TestimonialWrapper from '~/components/TestimonialWrapper';
-import '../../TestimonialWrapper/TestimonialCard/data';
+import '~/components/TestimonialWrapper/TestimonialCard/data';
 import { data } from '~/components/TestimonialWrapper/TestimonialCard/data';
+
+import './testimonial-section.scss';
 
 export const TestimonialSection = () => {
   return (
