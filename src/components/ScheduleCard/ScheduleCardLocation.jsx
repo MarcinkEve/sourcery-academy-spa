d77414b89a7063e8a77035fceffb709f5eecf6a9
Arminas Strolys <arminas.strolys@gmail.com>
2022-12-22 19:29:35 +0200

    Dynamic separator does not gain height.

--- src/components/ScheduleCard/ScheduleCardLocation.jsx
+++ src/components/ScheduleCard/ScheduleCardLocation.jsx
@@ -7,13 +7,16 @@ import IconVerticalLine from '~/assets/icons/icon-vertical-line.svg';
 export const ScheduleCardLocation = ({ city, day, month }) => {
   return (
     <div className="location">
-      <div className="location__date">
-        <span className="location__date-month">{month}</span>
-        <span className="location__date-day">{day}</span>
-      </div>
+      {month && day && (
+        <div className="location__date">
+          <span className="location__date-month">{month}</span>
+          <span className="location__date-day">{day}</span>
+        </div>
+      )}
+
       {/* <IconVerticalLine className="location__separator" /> */}
       <div className="location__separator"></div>
-      <IconLocation className="location__pin" />
+      {city && <IconLocation className="location__pin" />}
       <span className="location__city">{city}</span>
     </div>
   );
