221f2b363b5dd7ce650f6ef61257016cd7aaa48e
KlemBut <klemensasbutkevicius@gmail.com>
2023-01-02 12:22:56 +0200

    minor fixes

--- src/pages/AcademiesPages/AcademyWrapper/AdmissionSection/AdmissionSection.jsx
+++ src/pages/AcademiesPages/AcademyWrapper/AdmissionSection/AdmissionSection.jsx
@@ -11,21 +11,23 @@ import './admission-section';
 
 export const AdmissionSection = ({ data }) => {
   return (
-    <section className="admission-section">
-      <div className="admission-section__text-container">
-        <Admission content={data.admission} />
-      </div>
-      <div className="admission-section__particles-container">
-        <Particles className="admission-section__particles" />
-      </div>
-      <div className="admission-section__dates-container">
-        <DatesContainer title="Dates" cardData={data.dates} />
-        <Line className="admission-section__line" />
-      </div>
-      <div className="admission-section__image-container">
-        <Image className="admission-section__image" />
-      </div>
-    </section>
+    data && (
+      <section className="admission-section">
+        <div className="admission-section__text-container">
+          <Admission content={data.admission} />
+        </div>
+        <div className="admission-section__particles-container">
+          <Particles className="admission-section__particles" />
+        </div>
+        <div className="admission-section__dates-container">
+          <DatesContainer title="Dates" cardData={data.dates} />
+          <Line className="admission-section__line" />
+        </div>
+        <div className="admission-section__image-container">
+          <Image className="admission-section__image" />
+        </div>
+      </section>
+    )
   );
 };
 
