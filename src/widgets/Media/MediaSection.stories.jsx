08c6063f8b91f5cf851647626085dffc00fc8cb8
Mantvydas Liobė <liobem@gmail.com>
2023-01-02 13:59:06 +0200

    different data for different theme mediaSection

--- src/widgets/Media/MediaSection.stories.jsx
+++ src/widgets/Media/MediaSection.stories.jsx
@@ -1,39 +1,10 @@
 import React from 'react';
 
 import { MediaSection } from './MediaSection';
+import { mediaLists } from './storybookData';
 
 const titleForTheme = (theme) =>
   `${theme === 'Home' ? `${theme} page` : `Sourcery for ${theme}`}`;
-
-const mediaList = [
-  {
-    academy: 'developers',
-    type: 'image',
-    thumbnail:
-      'https://sfe-2022-data.netlify.app/static/images/developers/thumbnails/P1190684-2.jpg',
-    src:
-      'https://sfe-2022-data.netlify.app/static/images/developers/P1190684-2.jpg',
-    alt: '',
-  },
-  {
-    academy: 'testers',
-    type: 'image',
-    thumbnail:
-      'https://sfe-2022-data.netlify.app/static/images/testers/thumbnails/48670590916_a5d218a46c_o.jpg',
-    src:
-      'https://sfe-2022-data.netlify.app/static/images/testers/48670590916_a5d218a46c_o.jpg',
-    alt: '',
-  },
-  {
-    academy: 'frontend',
-    type: 'video',
-    thumbnail:
-      'https://sfe-2022-data.netlify.app/static/video/frontend/thumbnails/49397671398_59fa405f03_o-3.jpg',
-    src:
-      'https://sfe-2022-data.netlify.app/static/video/frontend/cb72a22297be0376bb14f23b598b8c77cc42c476.mp4',
-    alt: '',
-  },
-];
 
 export default {
   title: 'widgets/MediaSectionComponent',
@@ -49,22 +20,22 @@ export default {
         home: {
           theme: 'home',
           title: titleForTheme('Home'),
-          mediaList,
+          mediaList: mediaLists.home,
         },
         developers: {
           theme: 'developers',
           title: titleForTheme('Developers'),
-          mediaList,
+          mediaList: mediaLists.developers,
         },
         testers: {
           theme: 'testers',
           title: titleForTheme('Testers'),
-          mediaList,
+          mediaList: mediaLists.testers,
         },
         'front-end': {
           theme: 'front-end',
           title: titleForTheme('Front-End'),
-          mediaList,
+          mediaList: mediaLists.frontEnd,
         },
       },
     },
---
+++ src/widgets/Media/storybookData.js
@@ -0,0 +1,118 @@
+export const mediaLists = {
+  home: [
+    {
+      academy: 'developers',
+      type: 'image',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/images/developers/thumbnails/P1190684-2.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/images/developers/P1190684-2.jpg',
+      alt: '',
+    },
+    {
+      academy: 'testers',
+      type: 'image',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/images/testers/thumbnails/48670590916_a5d218a46c_o.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/images/testers/48670590916_a5d218a46c_o.jpg',
+      alt: '',
+    },
+    {
+      academy: 'frontend',
+      type: 'video',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/video/frontend/thumbnails/49397671398_59fa405f03_o-3.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/video/frontend/cb72a22297be0376bb14f23b598b8c77cc42c476.mp4',
+      alt: '',
+    },
+  ],
+  developers: [
+    {
+      academy: 'developers',
+      type: 'image',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/images/developers/thumbnails/47847358182_3c3e9b6cb9_o.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/images/developers/47847358182_3c3e9b6cb9_o.jpg',
+      alt: '',
+    },
+    {
+      academy: 'testers',
+      type: 'image',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/images/developers/thumbnails/52108036377_2d369922a9_o.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/images/developers/52108036377_2d369922a9_o.jpg',
+      alt: '',
+    },
+    {
+      academy: 'frontend',
+      type: 'video',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/video/developers/thumbnails/47110170574_c00cc2b54d_k-1.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/video/developers/80b0058a9428314582a3f25f3b1dfb8bc27de8a4.mp4',
+      alt: '',
+    },
+  ],
+  testers: [
+    {
+      academy: 'testers',
+      type: 'image',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/images/testers/thumbnails/41852970895_89c935092b_o.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/images/testers/41852970895_89c935092b_o.jpg',
+      alt: '',
+    },
+    {
+      academy: 'testers',
+      type: 'image',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/images/testers/thumbnails/47971633168_62046b153a_k-2.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/images/testers/47971633168_62046b153a_k-2.jpg',
+      alt: '',
+    },
+    {
+      academy: 'testers',
+      type: 'video',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/video/testers/thumbnails/48741988306_5beb53f25e_o.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/video/testers/7cd88093664cd782e4868a6706f2787da2eb7dc9.mp4',
+      alt: '',
+    },
+  ],
+  frontEnd: [
+    {
+      academy: 'frontend',
+      type: 'image',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/images/frontend/thumbnails/48972576503_33a1468130_o-1.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/images/frontend/48972576503_33a1468130_o-1.jpg',
+      alt: '',
+    },
+    {
+      academy: 'frontend',
+      type: 'image',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/images/frontend/thumbnails/48973132416_0f70527598_4k.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/images/frontend/48973132416_0f70527598_4k.jpg',
+      alt: '',
+    },
+    {
+      academy: 'frontend',
+      type: 'video',
+      thumbnail:
+        'https://sfe-2022-data.netlify.app/static/video/frontend/thumbnails/49397671398_59fa405f03_o-3.jpg',
+      src:
+        'https://sfe-2022-data.netlify.app/static/video/frontend/cb72a22297be0376bb14f23b598b8c77cc42c476.mp4',
+      alt: '',
+    },
+  ],
+};
