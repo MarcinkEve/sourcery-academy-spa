cfb6a2f2ef2a48c5d0a3a5eeb17ce9322c6e3400
MarcinkEve <marcinke@gmail.com>
2023-01-03 11:26:25 +0200

    destructured data and inserted line before routes import

--- src/pages/AcademiesPages/AcademyWrapper/AcademyLandingSection/AcademyLandingSection.jsx
+++ src/pages/AcademiesPages/AcademyWrapper/AcademyLandingSection/AcademyLandingSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shape, string } from 'prop-types';
-import { ROUTES } from '~/constants/routes';
 
+import { ROUTES } from '~/constants/routes';
 import TextSection from '~/components/UI/TextSection';
 import CornerVideo from '~/widgets/CornerVideo';
 import CornerLine from '~/assets/decorators/lines/landing-section-corner-line.svg';
@@ -10,12 +10,19 @@ import ParagraphLine from '~/assets/decorators/lines/landing-section-paragraph-l
 import './academy-landing-section.scss';
 
 export const AcademyLandingSection = ({ data }) => {
+  const {
+    title,
+    text,
+    videoSrc,
+    image: { src, alt },
+  } = data;
+
   return (
     <section className="academy-landing-section">
       <div className="academy-landing-section__paragraph">
         <TextSection
-          headingContent={<h1>{data.title}</h1>}
-          paragraphContent={<p>{data.text}</p>}
+          headingContent={<h1>{title}</h1>}
+          paragraphContent={<p>{text}</p>}
           buttonText={'Register'}
           pageRoute={ROUTES.APPLICATION}
         />
@@ -24,9 +31,9 @@ export const AcademyLandingSection = ({ data }) => {
       <div className="academy-landing-section__corner">
         <CornerLine className="academy-landing-section__corner-line" />
         <CornerVideo
-          image={data.image.src}
-          description={data.image.alt}
-          videoSrc={data.videoSrc}
+          image={src}
+          description={alt}
+          videoSrc={videoSrc}
           className="academy-landing-section__corner-video"
         />
       </div>
