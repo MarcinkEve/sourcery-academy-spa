2413dd05e463781daf12dcc5fd6c67cf076d9519
Arminas Strolys <arminas.strolys@gmail.com>
2023-01-03 20:49:58 +0200

    Finalized schedule card section mobile responsive breakpoint transformation. Fixed small issue with footer responsive also.

--- src/pages/AcademiesPages/AcademyWrapper/ScheduleSection/ScheduleCardsWrapper/ScheduleColumn/ScheduleColumn.jsx
+++ src/pages/AcademiesPages/AcademyWrapper/ScheduleSection/ScheduleCardsWrapper/ScheduleColumn/ScheduleColumn.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { scheduleColumnType } from '~/pages/AcademiesPages/AcademyWrapper/ScheduleSection/ScheduleCardsWrapper/types';
 import ScheduleSubcolumn from '~/pages/AcademiesPages/AcademyWrapper/ScheduleSection/ScheduleCardsWrapper/ScheduleSubcolumn';
+import { Button } from '~/components/Form/Button/Button';
 
 import './schedule-column.scss';
 
 export const ScheduleColumn = ({ columnData }) => {
+  const allColumnData = [...columnData.column_1, ...columnData.column_2];
+  const [isExpanded, setIsExpanded] = useState(true);
+
   return (
     <>
       <h3 className="schedule-column__title">{columnData.title.join(' / ')}</h3>
       <div className="schedule-column__data">
-        <ScheduleSubcolumn subcolumnData={columnData.column_1} />
-        {columnData.column_2 && (
-          <ScheduleSubcolumn subcolumnData={columnData.column_2} />
-        )}
+        <div className="schedule-column__large-screen">
+          <ScheduleSubcolumn subcolumnData={columnData.column_1} />
+          {columnData.column_2.length !== 0 && (
+            <ScheduleSubcolumn subcolumnData={columnData.column_2} />
+          )}
+        </div>
+
+        <div className="schedule-column__small-screen">
+          <ScheduleSubcolumn
+            subcolumnData={
+              isExpanded
+                ? allColumnData.map((column) => column).slice(0, 2)
+                : allColumnData.map((column) => column)
+            }
+          />
+
+          {allColumnData.length > 2 && (
+            <div className="schedule-column__button-container">
+              <Button
+                handleClick={() => setIsExpanded((prev) => !prev)}
+                label={`Show ${isExpanded ? 'More' : 'Less'}`}
+              />
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
