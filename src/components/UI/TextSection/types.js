144206979e5c71d348d80e59c69e6e62edafd3f6
Elena Darškuvienė <e.darskuviene@gmail.com>
2023-01-04 10:57:13 +0200

    minor mistype fix

--- src/components/UI/TextSection/types.js
+++ src/components/UI/TextSection/types.js
@@ -10,5 +10,5 @@ export const textSectionType = {
   paragraphContent: string.isRequired,
   buttonText: string,
   pageRoute: oneOf([...Object.values(ROUTES)]),
-  sectionId: string,
+  targetSectionId: string,
 };
