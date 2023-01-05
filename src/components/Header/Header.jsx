617662c2d1d7e46bf3327b2eb1048a5d012e3d47
Arminas Strolys <arminas.strolys@gmail.com>
2023-01-05 22:36:00 +0200

    Added burger on/off 'icon'. Modeled heading to go in center of page on pop-up layout when clicked on burger.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ import classNames from 'classnames';
 import SvgArrow from '~/assets/icons/icon-arrow-down.svg';
 import SvgLogo from '~/assets/icons/icon-logo.svg';
 import HeaderDropdown from '~/components/Header/Dropdown';
-import BurgerMenu from '~/components/Header/BurgerMenu';
 
+import './BurgerMenu/burgerMenu.scss';
 import './header.scss';
 
 const dropdownElements = [
@@ -43,6 +43,10 @@ export const Header = () => {
     return academiesPathArray;
   };
 
+  const handleClose = () => {
+    setIsBurgerMenuOpen(false);
+  };
+
   const academiesPaths = getAcademiesPath();
   const isAcademiesPathActive = (pathname) => academiesPaths.includes(pathname);
 
@@ -60,16 +64,34 @@ export const Header = () => {
 
   return (
     <>
-      {/* <BurgerMenu></BurgerMenu> */}
-      <BurgerMenu />
-      <div className="header">
+      <div
+        className="burger-menu"
+        onClick={() => setIsBurgerMenuOpen((prev) => !prev)}
+      >
+        <span
+          className={classNames(
+            'burger-menu__line',
+            isBurgerMenuOpen && 'burger-menu__line--active'
+          )}
+        ></span>
+      </div>
+      <div
+        className={classNames('header', isBurgerMenuOpen && 'header--hidden')}
+        // className={classNames('header', isBurgerMenuOpen && 'header--hidden')}
+      >
         <NavLink className="logo" to="/" aria-label="Home link">
           <SvgLogo className="logo__image" />
           <span className="logo__text">Sourcery Academy</span>
         </NavLink>
-        <ul className="navlinks">
+        <ul
+          className={classNames(
+            'navlinks',
+            isBurgerMenuOpen && 'navlinks--hidden'
+          )}
+        >
           <li>
             <NavLink
+              onClick={handleClose}
               className={({ isActive }) =>
                 `navlinks__link ${isActive ? 'navlinks__link--active' : ''}`
               }
@@ -91,7 +113,10 @@ export const Header = () => {
               <SvgArrow className="navlinks__academies-arrow" />
             </button>
             {isDropdownOpen && (
-              <div className="navlinks__academies-dropdown">
+              <div
+                onClick={handleClose}
+                className="navlinks__academies-dropdown"
+              >
                 <HeaderDropdown
                   data={dropdownElements}
                   onClickOutside={() => setIsDropdownOpen(false)}
@@ -101,6 +126,7 @@ export const Header = () => {
           </li>
           <li>
             <NavLink
+              onClick={handleClose}
               className={({ isActive }) =>
                 `navlinks__link ${isActive ? 'navlinks__link--active' : ''}`
               }
@@ -111,6 +137,7 @@ export const Header = () => {
           </li>
           <li>
             <NavLink
+              onClick={handleClose}
               className={({ isActive }) =>
                 `navlinks__link ${isActive ? 'navlinks__link--active' : ''}`
               }
@@ -121,6 +148,7 @@ export const Header = () => {
           </li>
           <li>
             <NavLink
+              onClick={handleClose}
               className={({ isActive }) =>
                 `navlinks__link ${isActive ? 'navlinks__link--active' : ''}`
               }
