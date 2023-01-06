6c1f7c259c618fb964050ac31aafc3d71342d204
MarcinkEve <marcinke@gmail.com>
2023-01-06 19:41:45 +0200

    removed ExtraWrappingSection, hardcoded data, removed data.json file, minor scss updates, Kids page removed

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import { string } from 'prop-types';
 
 import PageLayout from '~/layouts/pageLayout';
-import ExtraWrappingSection from '~/layouts/ExtraWrappingSection';
 import NotFoundContent from '~/pages/PageNotFound/PageNotFoundContent';
-
-import pageNotFoundData from './pageNotFoundData.json';
 
 export const PageNotFound = ({ theme }) => {
   return (
     <PageLayout theme={theme}>
-      <ExtraWrappingSection>
-        <NotFoundContent data={pageNotFoundData} />
-      </ExtraWrappingSection>
+      <NotFoundContent />
     </PageLayout>
   );
 };
--- src/pages/PageNotFound/PageNotFoundContent/PageNotFoundContent.jsx
+++ src/pages/PageNotFound/PageNotFoundContent/PageNotFoundContent.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
-import { shape } from 'prop-types';
 
 import { ROUTES } from '~/constants/routes';
 import TextSection from '~/components/UI/TextSection';
-import { textSectionType } from '~/components/UI/TextSection/types';
 import Image from '~/assets/images/testers-description.svg';
 
 import './page-not-found-content.scss';
 
-export const PageNotFoundContent = ({ data }) => {
-  const { headingContent, paragraphContent, buttonText } = data;
-
+export const PageNotFoundContent = () => {
   return (
     <div className="page-not-found">
       <TextSection
-        className="page-not-found__text"
-        headingContent={<h1>{headingContent}</h1>}
+        headingContent={<h1>{'404 - Page Not Found'}</h1>}
         isHeadingSpacingLarge
-        paragraphContent={<p>{paragraphContent}</p>}
-        buttonText={buttonText}
+        paragraphContent={
+          <p>{'Junior developers are still building this page!'}</p>
+        }
+        buttonText={'Back to Homepage'}
         pageRoute={ROUTES.HOME}
       />
       <Image className="page-not-found__image" />
     </div>
   );
-};
-
-PageNotFoundContent.propTypes = {
-  data: shape(textSectionType).isRequired,
 };
