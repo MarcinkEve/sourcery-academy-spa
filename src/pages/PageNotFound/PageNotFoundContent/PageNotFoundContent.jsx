dfe19a0e93d15569797fad725b0034d78f595e81
MarcinkEve <marcinke@gmail.com>
2023-01-11 17:59:25 +0200

    responsive design with current image

--- src/pages/PageNotFound/PageNotFoundContent/PageNotFoundContent.jsx
+++ src/pages/PageNotFound/PageNotFoundContent/PageNotFoundContent.jsx
@@ -10,12 +10,7 @@ export const PageNotFoundContent = () => {
   return (
     <div className="page-not-found">
       <TextSection
-        headingContent={
-          <h1>
-            <span className="page-not-found__heading-span">🕵️‍♂️</span>
-            {'404 - Page Not Found'}
-          </h1>
-        }
+        headingContent={<h1>{'404 - Page Not Found'}</h1>}
         isHeadingSpacingLarge
         paragraphContent={<p>{'Junior developers are on it!'}</p>}
         buttonText={'Go to Homepage'}
