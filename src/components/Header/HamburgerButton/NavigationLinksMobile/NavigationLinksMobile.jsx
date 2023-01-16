fadde7c403551e0779a1b54d0b091ac5c02b74de
Arminas Strolys <arminas.strolys@gmail.com>
2023-01-16 14:15:08 +0200

    Fixed bugs after QA

--- src/components/Header/HamburgerButton/NavigationLinksMobile/NavigationLinksMobile.jsx
+++ src/components/Header/HamburgerButton/NavigationLinksMobile/NavigationLinksMobile.jsx
@@ -20,19 +20,15 @@ export const NavigationLinksMobile = ({ handleClose, navigationLinks }) => {
               <>
                 <li
                   onClick={() => setShowDropDown((prev) => !prev)}
-                  className="navlinks-mobile__link"
+                  className={classNames(
+                    'navlinks-mobile__academies-link',
+                    link.dropdownElements
+                      .map((route) => route.route)
+                      .includes(pathname) && 'navlinks-mobile__link--active'
+                  )}
                 >
-                  <span
-                    className={classNames(
-                      'navlinks-mobile__academies-link',
-                      link.dropdownElements
-                        .map((route) => route.route)
-                        .includes(pathname) && 'navlinks-mobile__link--active'
-                    )}
-                  >
-                    Academies
-                    <SvgArrow className="navlinks-mobile__arrow-icon" />
-                  </span>
+                  {link.title}
+                  <SvgArrow className="navlinks-mobile__arrow-icon" />
                 </li>
 
                 {showDropDown &&
